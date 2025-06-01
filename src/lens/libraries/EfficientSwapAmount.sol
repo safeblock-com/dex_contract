e3401c8ff7c028559492076ccaaf8e6929984565
ElawBek <kdmitrii86@gmail.com>
2025-06-01 17:39:10 +0300

    fix args count

--- src/lens/libraries/EfficientSwapAmount.sol
+++ src/lens/libraries/EfficientSwapAmount.sol
@@ -4,15 +4,13 @@ pragma solidity ^0.8.0;
 import { IUniswapPool } from "../../interfaces/IUniswapPool.sol";
 
 import { HelperLib } from "./HelperLib.sol";
-import { HelperV3Lib } from "./HelperV3Lib.sol";
-import { Sqrt } from "../../libraries/Sqrt.sol";
+import { HelperV3Lib, Slot0, SwapCache, SwapState, FeeAndTickSpacing, TickMath, E18 } from "./HelperV3Lib.sol";
+
+import { FullMath } from "./uni3Libs/FullMath.sol";
 
 import { PoolHelper } from "../../facets/libraries/PoolHelper.sol";
 
 library EfficientSwapAmount {
-    uint256 internal constant FEE_DENOMINATOR = 1e6;
-    uint256 internal constant E18 = 1e18;
-
     // =========================
     // errors
     // =========================
@@ -28,14 +26,13 @@ library EfficientSwapAmount {
     /// @param pair The pair contract address
     /// @param tokenIn The address of the token to swap
     /// @param feeE6 The fee to be applied to the swap
-    /// @param targetPriceSlippage The target price slippage to be reached
     /// @return amountIn The amount of input token to swap
     /// @return amountOut The amount of output token to swap
     function efficientV2Amounts(
         IUniswapPool pair,
         address tokenIn,
-        uint256 feeE6,
-        uint256 targetPriceSlippage
+        uint256 targetPriceImpact,
+        uint256 feeE6
     )
         internal
         view
@@ -46,61 +43,97 @@ library EfficientSwapAmount {
             PoolHelper.getReserves({ pair: pair, tokenInIsToken0: tokenInIsToken0 });
 
         unchecked {
-            uint256 targetPrice = reserveOutput * E18 / reserveInput * (E18 - targetPriceSlippage) / E18;
+            uint256 low = 1;
+            uint256 high = reserveInput * 10;
 
-            uint256 k = reserveInput * reserveOutput; // Constant product
-            // Target reserveOut needed to reach targetPrice
-            uint256 newReserveInSquared = (k * E18) / targetPrice; // Scale by 1e18 to handle precision
-            uint256 newReserveIn = Sqrt.sqrt(newReserveInSquared);
+            // Spot price: reserveOut / reserveIn
+            uint256 currentPrice = reserveOutput * E18 / reserveInput;
 
-            if (newReserveIn <= reserveInput) {
-                revert EfficientSwapAmount_TargetPriceIsAlreadyReached();
+            while (low <= high) {
+                amountIn = (low + high) >> 1;
+
+                if (amountIn == 0) {
+                    return (0, 0);
+                }
+
+                // Execution price: amountOut / amountIn
+                amountOut = HelperLib.getAmountOut({
+                    amountIn: amountIn,
+                    reserveIn: reserveInput,
+                    reserveOut: reserveOutput,
+                    feeE6: feeE6
+                });
+
+                uint256 execPrice = amountOut * E18 / amountIn;
+
+                // Price impact: (currentPrice - execPrice) / currentPrice
+                uint256 priceImpact = (currentPrice - execPrice) * E18 / currentPrice;
+
+                if (priceImpact <= targetPriceImpact) {
+                    break;
+                } else {
+                    high = amountIn - 1;
+                }
             }
-
-            uint256 deltaX = newReserveIn - reserveInput; // Amount needed before fee adjustment
-
-            // Adjust for fee: amountIn = deltaX / (fee denominator - fee)
-            amountIn = (deltaX * FEE_DENOMINATOR) / (FEE_DENOMINATOR - feeE6);
         }
-
-        amountOut = HelperLib.getAmountOut({
-            amountIn: amountIn,
-            reserveIn: reserveInput,
-            reserveOut: reserveOutput,
-            feeE6: feeE6
-        });
     }
 
-    /// @notice Computes the input amount needed to reach a target price
-    /// @param pool The pool contract address
-    /// @param tokenIn The address of the token to swap
-    /// @param targetPriceSlippage The target price slippage to be reached
-    /// @return amountIn The amount of input token to swap
-    /// @return amountOut The amount of output token to swap
     function efficientV3Amounts(
         IUniswapPool pool,
         address tokenIn,
-        uint256 targetPriceSlippage
+        uint256 targetPriceImpact
     )
         internal
         view
         returns (uint256 amountIn, uint256 amountOut)
     {
-        (uint160 sqrtPriceX96,,) = HelperV3Lib.getSlot0({ pool: pool });
-
         (bool zeroForOne,) = PoolHelper.validateTokenInPair({ pool: pool, token: tokenIn });
+        Slot0 memory slot0Start;
+        SwapCache memory cache;
+        SwapState memory state;
+        FeeAndTickSpacing memory feeAndTickSpacing;
 
-        unchecked {
-            (amountIn, amountOut) = HelperV3Lib.quoteV3({
+        HelperV3Lib.getDataForSimulate(pool, slot0Start, cache, state, feeAndTickSpacing, 0);
+
+        uint256 currentPrice =
+            HelperV3Lib.calculatePrice({ sqrtPriceX96: slot0Start.sqrtPriceX96, zeroForOne: zeroForOne });
+
+        uint256 low = 1;
+        uint256 high = HelperV3Lib.calculateVirtualReserve({
+            sqrtPriceX96: slot0Start.sqrtPriceX96,
+            liquidity: cache.liquidityStart,
+            zeroForOne: zeroForOne
+        });
+
+        while (low <= high) {
+            uint256 mid = (low + high) >> 1;
+
+            (amountIn, amountOut) = HelperV3Lib.efficientQuoteV3({
                 pool: pool,
                 zeroForOne: zeroForOne,
-                amountSpecified: int256(uint256(type(uint112).max)),
-                sqrtPriceLimitX96: uint160(
-                    zeroForOne
-                        ? sqrtPriceX96 * (E18 - targetPriceSlippage) / E18
-                        : sqrtPriceX96 * (1e18 + targetPriceSlippage) / 1e18
-                )
+                amountSpecified: int256(mid),
+                sqrtPriceLimitX96: zeroForOne ? TickMath.MIN_SQRT_RATIO + 1 : TickMath.MAX_SQRT_RATIO - 1,
+                slot0Start: slot0Start,
+                cache: cache,
+                state: state,
+                feeAndTickSpacing: feeAndTickSpacing
             });
+
+            if (amountOut == 0 || amountIn == 0) {
+                high = mid - 1;
+                continue;
+            }
+
+            uint256 execPrice = FullMath.mulDiv(amountOut, E18, amountIn);
+
+            // Price impact: (currentPrice - execPrice) / currentPrice
+            uint256 priceImpact = (currentPrice - execPrice) * E18 / currentPrice;
+
+            if (priceImpact <= targetPriceImpact) {
+                break;
+            } else {
+                high = mid - 1;
+            }
         }
     }
 }
